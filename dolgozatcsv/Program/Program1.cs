e72809a0dd9059ea5ded0eda9eca3f69fa62953f
Krizsicskomark <151917177+Krizsicskomark@users.noreply.github.com>
2024-04-10 10:10:11 +0200

    Add files via upload

---
+++ dolgozatcsv/Program/Program1.cs
@@ -0,0 +1,34 @@
+﻿using Library.Data;
+using Library.Models;
+using System;
+
+namespace Program
+{
+    public class Program1
+    {
+        static void KiIr(IEnumerable<object> adatok)
+        {
+            foreach (var item in adatok)
+                Console.WriteLine(item.ToString());
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+
+            AdatContext db = new AdatContext();
+
+            if (!db.Adatok.Any())
+            {
+                var sorok = File.ReadAllLines(@"c:\Users\Diak\KM\dolgozat\1.csv").Skip(1);
+                foreach (var line in sorok)
+                {
+                    db.Adatok.Add(new Adatok(line));
+                    db.SaveChanges();
+                }
+            }
+
+            KiIr(db.Adatok);
+        }
+    }
+}
---
+++ dolgozatcsv/dolgozatcsv/Data/AdatContext.cs
@@ -0,0 +1,26 @@
+﻿using dolgozatcsv.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data
+{
+    public class AdatContext : DbContext
+    {
+        public AdatContext()
+        {
+        }
+        public DbSet<Adatok> Adatok { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+            string connMySql = "Server=127.0.0.1;User ID=root;Password=;Database=Nyilvantartas;";
+            optionsBuilder.UseMySql(connMySql, ServerVersion.AutoDetect(connMySql));
+        }
+
+    }
+}
